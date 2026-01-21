5c74baa053fca2636e8ec5f81f3e2a72a1b16307
Suridosa <Suridosa@DESKTOP-8HC16UE>
2026-01-21 22:33:04 +0900

    .

--- src/main/java/kr/co/trito/pdf/util/Base64Util.java
+++ src/main/java/kr/co/trito/pdf/util/Base64Util.java
@@ -1,16 +1,26 @@
 package kr.co.trito.pdf.util;
 
-import javax.xml.bind.DatatypeConverter;
+//import javax.xml.bind.DatatypeConverter;
+
+import org.apache.commons.codec.binary.Base64;
 
 public class Base64Util {
 
     public static String encode(byte[] bytes) {
-        return DatatypeConverter.printBase64Binary(bytes);
+        return new String(Base64.encodeBase64(bytes));
     }
 
     public static byte[] decode(String encodeStr) {
-    	return DatatypeConverter.parseHexBinary(encodeStr);
+    	return Base64.decodeBase64(encodeStr);
     }
+
+//    public static String encode(byte[] bytes) {
+//    	return DatatypeConverter.printBase64Binary(bytes);
+//    }
+//
+//    public static byte[] decode(String encodeStr) {
+//    	return DatatypeConverter.parseHexBinary(encodeStr);
+//    }
 
 
 }
