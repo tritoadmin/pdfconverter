d9972d660c0744b43acaaabd29788bae2042bc77
Suridosa <Suridosa@DESKTOP-8HC16UE>
2026-01-28 22:11:24 +0900

    .

--- src/main/java/kr/co/trito/pdf/config/WebConfig.java
+++ src/main/java/kr/co/trito/pdf/config/WebConfig.java
@@ -29,7 +29,7 @@ public class WebConfig implements WebMvcConfigurer {
     // Interceptor에서 제외되는 URL 주소
     private static final String[] EXCLUDE_PATHS = {
             "/",
-            "/**/tokens.do"
+            "/**/token.do"
     };
 
     @Override
